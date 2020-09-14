43862e2f5d4b80d047ac8810ec2ceb3d03e8d6e2
Khazri Mamedov <khazri.mamedov@gmail.com>
2020-09-14 11:00:01 +0300

    Find first digit

---
+++ first_digit/first_digit.cpp
@@ -0,0 +1,19 @@
+#include <system_error>
+#include <iostream>
+
+namespace dailyalgorithms
+{
+    // Instead of custom overflow checking, use list init for number
+    int first_digit(int number)
+    {
+        if (number < 0)
+        {
+            throw std::invalid_argument("Number must be > 0");
+        }
+        while(number > 10)
+        {
+            number = number / 10; 
+        }
+        return number;
+    }
+}
