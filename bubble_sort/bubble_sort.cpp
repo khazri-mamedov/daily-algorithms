b6e63db7693857ef60a6a8b854975215323e9716
Khazri Mamedov <khazri.mamedov@gmail.com>
2020-09-16 10:06:14 +0300

    Bubble sort

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+namespace dailyalgorithms
+{
+    using std::vector;
+
+    // O(n^2) complexity
+    template<typename T>
+    void sort(vector<T>& container)
+    {
+        for (size_t i = 0; i != container.size() - 1; ++i)
+        {
+            for (size_t j = 0; j != container.size() - i - 1; ++j)
+            {
+                if (container[j] > container[j + 1])
+                {
+                    std::swap(container[j], container[j + 1]);
+                }
+            }
+        }
+    }
+}
